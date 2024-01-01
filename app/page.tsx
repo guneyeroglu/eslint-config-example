fb9ec9e3358e13b00471e4f8b20d5b9a59407e68
guneyeroglu <benguneyeroglu@gmail.com>
2024-01-02 00:34:13 +0300

    config added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -76,4 +76,6 @@ export default function Home() {
       </div>
     </main>
   );
-}
+};
+
+export default Home;
